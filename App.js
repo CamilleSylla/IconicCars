9fbda3359e4259354775a376c352b6d077e4bc1a
Camille Sylla <camillesylla.web@gmail.com>
2020-10-07 04:56:26 +0200

    added new light

--- App.js
+++ App.js
@@ -21,43 +21,48 @@ function init() {
     const far = 5000;
     //Camera
     camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0,1.5,6);
+    camera.position.set(0, 1.2,8.5);
 
     //light
-    const ambient = new THREE.AmbientLight(0x404040, 1);
+    const ambient = new THREE.AmbientLight(0xffffff, 1);
     scene.add(ambient);
 
-    const light = new THREE.DirectionalLight(0xffffff, 3)
-    light.position.set(-2,20,5);
+    const light = new THREE.DirectionalLight(0xffffff, 2)
+    light.position.set(-25, 25, 2);
     light.castShadow = true;
     scene.add(light);
+    const light2 = new THREE.DirectionalLight(0xffffff, 2)
+    light2.position.set(25, 25, 2);
+    light2.castShadow = true;
+    scene.add(light2);
 
 
     //Renderer
-    renderer = new THREE.WebGLRenderer({antialias: true, alpha: true})
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
     renderer.setSize(container.clientWidth, container.clientHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
 
     container.appendChild(renderer.domElement);
 
-    
+
     //Model
     let loader = new THREE.GLTFLoader();
-    loader.load("./Car/scene.gltf", function(gltf){
+    loader.load("./Car/scene.gltf", function (gltf) {
         const root = gltf.scene;
         scene.add(root);
         root.rotation.y = 0.6
         car = gltf.scene.children[0];
         animate();
     })
-    
 };
 
-function animate (){
+
+
+function animate() {
     renderer.render(scene, camera);
     car.rotation.z += 0.005
     requestAnimationFrame(animate);
-    
+
 }
 
 init();
